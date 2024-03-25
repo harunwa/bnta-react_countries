8d1867d2618d9592e50fa7f97cc9101d402bfd92
Harun Walizadeh <harunwalizadeh@gmail.com>
2024-03-25 16:19:24 +0000

    extension: added display detaails functionality

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,20 +1,35 @@
-import React from "react";
-import '../App.css';
+import React, { useState } from "react";
 
-const Country = ({country, handleAddToVisited, countriesVisited}) => {
+const Country = ({ country, handleAddToVisited, countriesVisited }) => {
+    const [showDetails, setShowDetails] = useState(false);
+
     const isVisited = countriesVisited.find(
         (visitedCountry) => visitedCountry.name.common === country.name.common
     );
-    
-    return(
-    <>
-    <p>{country.name.common}</p>
-    <p>{country.flag}</p>
-    {!isVisited && (
-                <button onClick={() => handleAddToVisited(country)}>Add To Visited!</button>
+
+    const toggleDetails = () => {
+        setShowDetails(!showDetails);
+    };
+
+    return (
+        <>
+                <p>{country.name.common}</p>
+                <img src={country.flags.png}/>
+                {!isVisited && (
+                    <button onClick={() => handleAddToVisited(country)}>Add To Visited!</button>
+                )}
+            {showDetails && (
+                <div>
+                    <p>Capital: {country.capital}</p>
+                    <p>Population: {country.population}</p>
+                    <p>Languages: {Object.values(country.languages).join(", ")}</p>
+                </div>
             )}
-    </>
-    )
-}
+            <button onClick={toggleDetails}>
+                {showDetails ? "Hide Details" : "Show Details"}
+            </button>
+        </>
+    );
+};
 
 export default Country;
