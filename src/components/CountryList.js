d108a29e8d55b523cbe59d2a008558be4910f3c3
Harun Walizadeh <harunwalizadeh@gmail.com>
2024-03-25 14:07:41 +0000

    added packages & starters

---
+++ src/components/CountryList.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Country from "./Country";
+
+const CountryList = () => {
+
+}
+
+export default CountryList;
---
+++ src/containers/CountryContainer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import CountryList from '../components/CountryList';
+
+const CountryContainer = () => {
+    <>
+    
+    
+    </>
+}
+
+export default CountryContainer;
